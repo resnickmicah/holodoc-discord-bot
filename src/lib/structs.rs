557fea54eac55a4302a50a7056e10cfd01cc9102
mfoman <frederikblunck@gmail.com>
2021-02-28 22:04:02 +0100

    Add global state. Kinda wonky.

--- src/lib/structs.rs
+++ src/lib/structs.rs
@@ -1,11 +1,11 @@
 use super::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Eateries {
     pub names: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Foods {
     pub healthy: Vec<String>,
     pub less_healthy: Vec<String>,
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,56 @@
 ======================================== */
 mod lib;
 use dotenv;
-use lib::commands::*;
+use lib::{
+    commands::*,
+    structs::{Eateries, Foods},
+};
 
 use rand::seq::SliceRandom;
 use serde::{Deserialize, Serialize};
 use serde_json as json;
-use serenity::async_trait;
-use serenity::client::{Client, Context, EventHandler};
 use serenity::framework::standard::{
     macros::{command, group},
     Args, CommandResult, Delimiter, StandardFramework,
 };
 use serenity::model::channel::Message;
+use serenity::{async_trait, prelude::TypeMapKey};
+use serenity::{
+    client::{Client, Context, EventHandler},
+    prelude::RwLock,
+};
 
-use std::fs;
+use std::{collections::HashMap, fs, sync::Arc};
 use std::{env, path::Path};
 
 /* ========================================
  * Consts
 ======================================== */
 const BOT_PREFIX: &str = "!!";
+
+#[macro_use]
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+/* ========================================
+ * State
+======================================== */
+struct FoodsState;
+impl TypeMapKey for FoodsState {
+    // We need to implement typemapkey to avoid the bot deadlocking.
+    type Value = Arc<RwLock<HashMap<String, Foods>>>;
+}
+
+struct EateryState;
+impl TypeMapKey for EateryState {
+    // We need to implement typemapkey to avoid the bot deadlocking.
+    type Value = Arc<RwLock<HashMap<String, Eateries>>>;
+}
 
 /* ========================================
  * Program
@@ -51,6 +81,25 @@ async fn main() {
         .framework(framework)
         .await
         .expect("Error creating client");
+
+    // Initialize data
+    {
+        let foods = &fs::read_to_string(Path::new("./db/foods.json")).expect("Couldn't read file.");
+        let foods: Foods = json::from_str(foods).expect("Couldn't parse json.");
+
+        let eatery =
+            &fs::read_to_string(Path::new("./db/eatery.json")).expect("Couldn't read file.");
+        let eatery: Eateries = json::from_str(eatery).expect("Couldn't parse json.");
+
+        let mut data = client.data.write().await;
+
+        data.insert::<FoodsState>(Arc::new(RwLock::new(
+            hashmap!["foods".to_string() => foods],
+        )));
+        data.insert::<EateryState>(Arc::new(RwLock::new(
+            hashmap!["eatery".to_string() => eatery],
+        )));
+    }
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
